6fbe7c66eef1a5bffb8c1a8dd51ff4938b696a29
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-05-27 11:00:03 -0400

    remove unnecessary loop by storing totals in class, fixed a naming typo

--- src/liquidity_pool.py
+++ src/liquidity_pool.py
@@ -9,19 +9,23 @@ class LP:
         self.c2_name = coin2
         self.c1_amt = coin1_amt
         self.c2_amt = coin2_amt
+        self.c1_total = coin1_amt
+        self.c2_total = coin2_amt
         self.k = coin1_amt * coin2_amt
 
     def add_liquidity(self, amt_c1: float, amt_c2: float, adr: str):
         try:
             self.liquidity_providers[adr]["c1"] += amt_c1
-            self.liquidity_providers[adr]["c2"] += amt_c1
+            self.liquidity_providers[adr]["c2"] += amt_c2
         except:
             self.liquidity_providers[adr]["c1"] = amt_c1
-            self.liquidity_providers[adr]["c2"] = amt_c1
+            self.liquidity_providers[adr]["c2"] = amt_c2
         if self.c1_amt / self.c2_amt == amt_c1 / amt_c2:
             self.c1_amt += amt_c1
             self.c2_amt += amt_c2
             self.k = self.c1_amt * self.c2_amt
+            self.c1_total += amt_c1
+            self.c2_total += amt_c2
         else:
             raise Exception("not balanced ratio of amounts")
         return self.liquidity_providers[adr]
@@ -33,17 +37,15 @@ class LP:
         amt -= fee
 
         if name == self.c1_name:
-            total = sum(v["c1"] for _, v in self.liquidity_providers.items())
             for k, v in self.liquidity_providers.items():
-                self.liquidity_providers[k]["c1"] += fee * v["c1"] / total
+                self.liquidity_providers[k]["c1"] += fee * v["c1"] / self.c1_total
             return_amt = self.k / (self.c1_amt + amt) - self.c2_amt
             self.c1_amt += amt
             self.c2_amt -= return_amt
             return return_amt
         else:
-            total = sum(v["c2"] for _, v in self.liquidity_providers.items())
             for k, v in self.liquidity_providers.items():
-                self.liquidity_providers[k]["c2"] += fee * v["c2"] / total
+                self.liquidity_providers[k]["c2"] += fee * v["c2"] / self.c2_total
             return_amt = self.k / (self.c2_amt + amt) - self.c1_amt
             self.c2_amt += amt
             self.c1_amt -= return_amt
